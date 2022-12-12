4f4f8a032dce2dd25835d5418704d7bf503e50f6
Agnieszka Typiak <agnieszka.typiak@mailbox.org>
2022-12-12 13:15:41 +0100

    Rozwiazanie

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -1,11 +1,46 @@
 package pl.javastart.task;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class CoordinateSystem {
 
+public class CoordinateSystem {
+    String part;
     // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
-        System.out.println("Punkt (5, -3)");
+        scanner.useLocale(Locale.US);
+        System.out.println("Podaj X");
+        double x = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Podaj Y");
+        double y = scanner.nextDouble();
+        Point point = new Point(x,y);
+
+        if (isZero(point.x)) {
+            if (isZero(point.y)) {
+                part = "w środku układu współrzędnych";
+            } else part = "na osi X";
+        } else if (isZero(point.y) && !isZero(point.x)) {
+            part = "na osi Y";
+        } else if (lessThanZero(point.x)) {
+            if (lessThanZero(point.y)) {
+                part = "w III ćwiartce układu współrzędnych";
+            } else part = "w II ćwiartce układu współrzędnych";
+        } else if (!lessThanZero(point.x)) { //warunek x / y == 0 został już obsłużony, więc w tej części będą brane tylko punkty != 0
+            if (!lessThanZero(point.y)) {
+                part = "w I ćwiartce układu współrzędnych";
+            } else part = "w IV ćwiartce układu współrzędnych";
+        }
+
+        System.out.printf(Locale.US,"Punkt (%.1f, %.1f) leży %s", point.x, point.y, part);
     }
+
+    boolean isZero(double number) {
+        return (number == 0);
+    }
+
+    boolean lessThanZero(double number) {
+        return (number < 0);
+    }
+
 }
